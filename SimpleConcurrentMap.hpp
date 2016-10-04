5ac5a7331a25024aa167d96736cb36db2e6c2a1a
Matthew Milano <milano@cs.cornell.edu>
2016-10-04 11:32:57 -0400

    simple concurrent map

---
+++ SimpleConcurrentMap.hpp
@@ -0,0 +1,32 @@
+#pragma once
+#include <map>
+#include <shared_mutex>
+
+namespace mutils{
+
+	template<typename... A>
+	class SimpleConcurrentMap{
+
+		std::map<A...> map;
+		mutable std::shared_mutex mut;
+		using slock_t = std::shared_lock<std::shared_mutex>;
+		using ulock_t = std::unique_lock<std::shared_mutex>;
+	public:		
+		template<typename... T>
+		const auto& at(T&& ... t) const {
+			slock_t l{mut};
+			return map.at(std::forward<T>(t)...);
+		}
+		
+		template<typename... T>
+		auto& operator[](T&& ... t){
+			ulock_t l{mut};
+			return map.operator[](std::forward<T>(t)...);
+		}
+
+		template<typename... T>
+		auto count(T&& ...t) const {
+			return map.count(std::forward<T>(t)...);
+		}
+	};
+}
