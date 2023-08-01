e3dd51fdb6dcbe42532250b5d670b3e0f74f015c
Edward Tremel <high.order.bits@gmail.com>
2023-08-01 19:54:07 -0400

    Fix CMakeLists to properly configure mutils-containersConfig.cmake (#2)
    Using ${CMAKE_INSTALL_PREFIX} in mutils-containersConfig.cmake doesn't
    work because that variable is not expanded when "make install" is
    executed, so the config file that gets installed still has the literal
    string ${CMAKE_INSTALL_PREFIX} in it. Then when another CMake project
    uses find_package(mutils-containers), it loads
    mutils-containersConfig.cmake and replaces ${CMAKE_INSTALL_PREFIX} with
    whatever its current value is for that project, even if it's not the
    directory where mutils-containers was installed.
    
    This is the same bug fix described in PR #4 of the mutils repository
    (https://github.com/mpmilano/mutils/pull/4), just applied to
    mutils-containers. The CMakeLists incantations are almost exactly the
    same, except I used ${CMAKE_INSTALL_LIBDIR} in the ConfigPackageLocation
    variable to make it more portable, following an example I found online.

--- mutils-containersConfig.cmake
+++ mutils-containersConfig.cmake
@@ -1,1 +1,6 @@
-set(mutils-containers_INCLUDE_DIRS "${CMAKE_INSTALL_PREFIX}/include")
+@PACKAGE_INIT@
+
+set_and_check(mutils-containers_INCLUDE_DIRS "@PACKAGE_CMAKE_INSTALL_INCLUDEDIR@")
+include("${CMAKE_CURRENT_LIST_DIR}/mutils-containersTargets.cmake")
+
+check_required_components(mutils-containers)
